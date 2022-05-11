7c97987069154be1b6d1e2eeaaa47c4555b54c6e
kartik1430 <agrawalkartik455@gmail.com>
2022-05-11 16:38:24 +0530

    Authorize

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -8,25 +8,26 @@ import { navItems } from './_nav';
   templateUrl: './default-layout.component.html',
 })
 export class DefaultLayoutComponent implements OnInit {
-  
   public navItems = navItems;
-  displaymenu=false;
-  displayMaster=false;
-  currentRole:any;
+  displaymenu = false;
+  displayMaster = false;
+  currentRole: any;
   public perfectScrollbarConfig = {
     suppressScrollX: true,
   };
 
-  constructor( private service : ApiService) {}
+  constructor(private service: ApiService) {}
 
   ngOnInit(): void {
     this.MenuDisplay();
   }
 
-  MenuDisplay(){
-    if(this.service.GetToken()!=''){
-    this.currentRole = this.service.GetRolebyToken(this.service.GetToken());
-    this.displayMaster = this.currentRole==1;
+  MenuDisplay() {
+    if (this.service.GetToken() != '') {
+      this.currentRole = this.service.GetRolebyToken(this.service.GetToken());
+      console.log(this.currentRole);
+      this.displayMaster = this.currentRole == 1;
+      console.log(this.displayMaster);
+    }
   }
-}
 }
